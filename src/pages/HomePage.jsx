8fd6bae84a9efd059c926293adecfc6f35ea9ed9
ai-433729 <ai8692919@gmail.com>
2026-07-02 15:16:10 +0900

    0702

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -7,7 +7,7 @@ export default function HomePage() {
       <h1>Reactに関するクイズ</h1>
       <p>これはReactの勉強のために作成したページです。</p>
       <p>復習もかねてReactに関するクイズを5問出題します。</p>
-      <Link to={ROUTES.QUIZ} className="quiz-link">Start !!</Link>
+      <Link to={ROUTES.QUIZ} className="quiz-link">Start</Link>
     </>
   )
 }
